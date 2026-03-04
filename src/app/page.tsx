b374c2bf8e4fed6a4f4d087567b2250690a2f30b
Yael <243678@ids.upchiapas.edu.mx>
2026-03-03 21:55:15 -0600

    estilos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,14 +29,16 @@ export default function Home() {
   }
 
   return (
-    <>
-      <div>
-        <button type="button" onClick={handleFetchClick}>Obtener dato curioso</button>
-        <label htmlFor="nombre">Cantidad de datos curiosos:</label>
-        <input type="number" name="nombre" id="id" placeholder={meowFactsAmount.toString()} onChange={(e) => handleAmountClick(e.target.value.trim())} />
+    <div className="m-5">
+      <div className="flex gap-5 items-center mb-5">
+        <button type="button" onClick={handleFetchClick} className="px-5 py-2 rounded-2xl bg-purple-600">Obtener dato curioso</button>
+        <div>
+          <label htmlFor="nombre">Cantidad de datos curiosos:</label>
+          <input type="number" name="nombre" id="id" value={meowFactsAmount.toString()} onChange={(e) => handleAmountClick(e.target.value.trim())} />
+        </div>
       </div>
       {isLoading && <p>Cargando...</p>}
       {meowFacts && meowFacts.data.map( (fact:string, key:number) => <p key={key}>{fact}</p> )}
-    </>
+    </div>
   );
 }

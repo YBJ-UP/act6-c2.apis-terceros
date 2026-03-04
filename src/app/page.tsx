b4d05fb1334dbd58b34d98204e8b06812e2c9f11
Yael <243678@ids.upchiapas.edu.mx>
2026-03-03 21:46:48 -0600

    avances, funcionalidad del boton para elegir la cantidaad de datos curiosos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,13 @@ import { useState } from "react";
 export default function Home() {
 
   const [ meowFacts, setMeowFacts ] = useState<meowFactsResponse>({data:[]})
+  const [ meowFactsAmount, setMeowFactsAmount ] = useState<number>(1)
   const [ isLoading, setLoading ] = useState<boolean>(false)
 
-  async function handleClick(){
+  async function handleFetchClick(){
     try {
       setLoading(true)
-      const res = await fetchMeowFacts(2)
+      const res = await fetchMeowFacts(meowFactsAmount)
       setMeowFacts(res)
     } catch(error) {
       console.log(error)
@@ -21,9 +22,19 @@ export default function Home() {
     }
   }
 
+  function handleAmountClick(numero:string){
+    if (Number(numero) > 0) {
+      setMeowFactsAmount(Number(numero))
+    }
+  }
+
   return (
     <>
-      <button type="button" onClick={handleClick}>Obtener dato curioso</button>
+      <div>
+        <button type="button" onClick={handleFetchClick}>Obtener dato curioso</button>
+        <label htmlFor="nombre">Cantidad de datos curiosos:</label>
+        <input type="number" name="nombre" id="id" placeholder={meowFactsAmount.toString()} onChange={(e) => handleAmountClick(e.target.value.trim())} />
+      </div>
       {isLoading && <p>Cargando...</p>}
       {meowFacts && meowFacts.data.map( (fact:string, key:number) => <p key={key}>{fact}</p> )}
     </>

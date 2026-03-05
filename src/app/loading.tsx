027eb2181bd50b7fab6ff4a687b76715a06476b1
Yael <243678@ids.upchiapas.edu.mx>
2026-03-05 17:15:26 -0600

    se cambia el estilo para que se vea más

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function loading() {
     return (
-        <div>
+        <div className="flex flex-col items-center gap-5">
             <Image src={"/michibaile.gif"} alt="Cargando..." width={1000} height={500}/>
-            <p>Cargando...</p>
+            <h1 className="font-bold text-3xl">Cargando...</h1>
         </div>
     )
 }

84aa1099dd2982ba53953cdc7a839e785b8608e2
Yael <243678@ids.upchiapas.edu.mx>
2026-03-05 19:00:48 -0600

    se ponen los try catch, no se por que no los tenia

--- src/service/cataasService.ts
+++ src/service/cataasService.ts
@@ -15,9 +15,14 @@ export async function getRandomImage(message?:string): Promise<cataasResponse | 
 }
 
 export async function getGIF(): Promise<cataasResponse | errorType> {
-    const res = await fetch("https://cataas.com/cat/gif?json=true")
-    if (!res.ok) {
-        return { error:res.status, message:res.statusText }
+    try {
+        const res = await fetch("https://cataas.com/cat/gif?json=true")
+        if (!res.ok) {
+            return { error:res.status, message:res.statusText }
+            throw new Error("Error al conseguir los datos: " + res.status + "\n" + res.statusText) //yac q no funciona pero noc q hacerle
+        }
+        return res.json()
+    } catch (error:any) {
+        throw new Error(error)
     }
-    return res.json()
 }
--- src/service/meowfactsService.ts
+++ src/service/meowfactsService.ts
@@ -2,9 +2,13 @@ import { errorType } from "@/model/error";
 import meowFactsResponse from "@/model/meowfact";
 
 export async function fetchMeowFacts (number?:number):Promise<meowFactsResponse | errorType> {
-    const res = number? await fetch(`https://meowfacts.herokuapp.com/?lang=esp-mx&count=${number}`) : await fetch("https://meowfacts.herokuapp.com/?lang=esp-mx")
-    if (!res.ok) {
-        return { error:res.status, message:res.statusText }
+    try {
+        const res = number? await fetch(`https://meowfacts.herokuapp.com/?lang=esp-mx&count=${number}`) : await fetch("https://meowfacts.herokuapp.com/?lang=esp-mx")
+        if (!res.ok) {
+            return { error:res.status, message:res.statusText }
+        }
+        return res.json()
+    } catch (error:any) {
+        throw new Error(error)
     }
-    return res.json()
 }
